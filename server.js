4b8b36ac36069fe653a294b70fb1c5f99bc00adf
Renato Vasconcellos <revagomes@gmail.com>
2014-04-15 19:53:13 -0300

    Added some examples of file reading.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var index = fs.readFileSync('public/index.html');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
