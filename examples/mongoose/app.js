287e0e5842e4f385adbdf890d1bc34dcd46f505f
Renato Vasconcellos <revagomes@gmail.com>
2014-04-15 20:06:14 -0300

    Started the mongoose app.

--- examples/mongoose/app.js
+++ examples/mongoose/app.js
@@ -1,1 +1,13 @@
 // Mongoose App.
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', function(err){
+  console.log('Erro de conexao.', err)
+});
+
+db.once('open', function () {
+  console.log('Conexão aberta.')
+});
