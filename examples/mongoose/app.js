c914f2fc9c96e07ef7099d91f4c7f27bac234f51
Renato Vasconcellos <revagomes@gmail.com>
2014-04-15 21:56:34 -0300

    [WIP] Adjustments on App.js

--- examples/mongoose/app.js
+++ examples/mongoose/app.js
@@ -1,15 +1,37 @@
 // Mongoose App.
 
+var http = require("http");
 var Beer = require('./schema.js').Beer;
 
-var query = {name: {$regex: /heineken/i }};
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/pain"});
 
-var mod = {alcohol: 90};
+  var url = request.url;
 
-Beer.update(query, mod, {multi: true}, function (err, beer) {
-  if (err){
-    console.log('Erro: ', err);
-  }else{
-    console.log('The Beer was successfully updated!', beer);
+  if (url == '/beers/create') {
+    var dados = {
+      name: 'Skol',
+      description: 'Mijo de Rato',
+      alcohol: 79.5,
+      category: 'pilsen'
+    }
+
+    var model = new Beer(dados);
+
+    model.save(function (err) {
+      if (err) {
+        response.write('ERROR!!!');
+        console.log('Erro: ', err);
+      }
+      else {
+        var msg = 'Beer successfully inserted: ' + JSON.stringify(dados);
+        response.write(msg);
+        console.log(msg);
+      }
+      response.end();
+    });
   }
-});
+
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
